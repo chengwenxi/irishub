90e05d752e582d047c49cb166d76ce4d6e5fe7d6
chengwenxi <vincent.ch.cn@gmail.com>
2018-10-31 13:45:51 +0800

    IRISHUB-575: define refactor and init binding

--- modules/iservice/wire.go
+++ modules/iservice/wire.go
@@ -7,6 +7,7 @@ import (
 // Register concrete types on wire codec
 func RegisterWire(cdc *wire.Codec) {
 	cdc.RegisterConcrete(MsgSvcDef{}, "iris-hub/iservice/MsgSvcDef", nil)
+	cdc.RegisterConcrete(MsgSvcBind{}, "iris-hub/iservice/MsgSvcBinding", nil)
 }
 
 var msgCdc = wire.NewCodec()
